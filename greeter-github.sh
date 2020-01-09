6cda6e4e5a05d00bfe98a2860a2e8b73bbade737
akijakya <jakyandras@gmail.com>
2020-01-09 14:09:20 +0100

    deleted git config, simplified repo cloning

--- greeter-github.sh
+++ greeter-github.sh
@@ -3,15 +3,12 @@ sudo apt update
 
 # install git
 sudo apt install -yy git
-git config --global user.email "jakyandras@gmail.com"
-git config --global user.name "akijakya"
 
 # install Node
 sudo apt install -yy npm
 
 # cloning repo
-mkdir ~/greeter
-cd greeter
+cd ~
 git clone https://github.com/akijakya/docker-jenkins-test.git
 cd docker-jenkins-test
 npm install
