0d3a75361cf0ddf96f8d9d27c0314f969a82ebd0
akijakya <jakyandras@gmail.com>
2020-01-10 14:23:21 +0100

    added userData script

--- pulumi/index.js
+++ pulumi/index.js
@@ -21,10 +21,36 @@ let group = new aws.ec2.SecurityGroup("webserver-secgrp", {
     ],
 });
 
+let userData = 
+`#! /bin/bash
+
+# installing Docker
+sudo apt-get install -yy \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   bionic \
+   stable"
+sudo apt update
+sudo apt install -yy docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# pulling and starting the docker container
+sudo systemctl start docker
+sudo docker pull akijakya/docker-single-test:latest
+sudo docker run -d -p 80:3000 akijakya/docker-single-test:latest`;
+
 let server = new aws.ec2.Instance("webserver-www", {
     instanceType: size,
     securityGroups: [ group.name ], // reference the security group resource above
     ami: ami.id,
+    userData = userData,
 });
 
 exports.publicIp = server.publicIp;
