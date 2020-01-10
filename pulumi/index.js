bc3e978168a8905b46d4fc1c97b161c68bd8c3e3
akijakya <jakyandras@gmail.com>
2020-01-10 14:15:52 +0100

    added the specific ubuntu ami I would like to use

--- pulumi/index.js
+++ pulumi/index.js
@@ -1,10 +1,31 @@
 "use strict";
-const pulumi = require("@pulumi/pulumi");
+
 const aws = require("@pulumi/aws");
-const awsx = require("@pulumi/awsx");
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+let size = "t2.micro";     // t2.micro is available in the AWS free tier
+let ami = aws.getAmi({
+    filters: [{
+      name: "name",
+    //   values: ["amzn-ami-hvm-*"],
+      values: ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"],
+    }],
+    // owners: ["137112412989"], // This owner ID is Amazon
+    owners: ["099720109477"],
+    mostRecent: true,
+});
 
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+let group = new aws.ec2.SecurityGroup("webserver-secgrp", {
+    ingress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+});
+
+let server = new aws.ec2.Instance("webserver-www", {
+    instanceType: size,
+    securityGroups: [ group.name ], // reference the security group resource above
+    ami: ami.id,
+});
+
+exports.publicIp = server.publicIp;
+exports.publicHostName = server.publicDns;
+exports.amiName = ami.id;
